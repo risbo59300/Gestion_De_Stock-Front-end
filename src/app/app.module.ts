824f17381d2cb8f0e8920f364ca3a15b8f89ea30
ybgabamano <ybgabamano@gmail.com>
2022-08-11 00:05:02 +0200

    Creation de l'interface de connexion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PageLoginComponent } from './pages/page-login/page-login.component';
+import {RouterModule} from "@angular/router";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageLoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
