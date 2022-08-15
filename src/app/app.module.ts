ceeab2f6731967f6579e8db30bea6019735f939f
ybgabamano <ybgabamano@gmail.com>
2022-08-15 21:49:28 +0200

    Page d'inscription créer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { PageLoginComponent } from './pages/page-login/page-login.component';
 import {RouterModule} from "@angular/router";
+import { PageInscriptionComponent } from './pages/page-inscription/page-inscription.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PageLoginComponent
+    PageLoginComponent,
+    PageInscriptionComponent
   ],
   imports: [
     BrowserModule,
